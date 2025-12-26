3f750ab5880335a1dc4724a1d979e2512b7001dd
15506957109 <2284467180@qq.com>
2025-12-26 14:35:18 +0800

    添加分组校验宏能 JSR303

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/BrandEntity.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/BrandEntity.java
@@ -1,5 +1,7 @@
 package com.atguigu.gulimall.product.entity;
 
+import com.atguigu.common.valid.AddGroup;
+import com.atguigu.common.valid.UpdateGroup;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
@@ -25,18 +27,20 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 品牌id
 	 */
+    @NotNull(message = "修改品牌id不能为空", groups = {UpdateGroup.class})
+    @Null(message = "新增时品牌id不能指定ID", groups = {AddGroup.class})
 	@TableId
 	private Long brandId;
 	/**
 	 * 品牌名
 	 */
-    @NotBlank(message = "品牌名不能为空")
+    @NotBlank(message = "品牌名不能为空", groups = {AddGroup.class, UpdateGroup.class})
 	private String name;
 	/**
 	 * 品牌logo地址
 	 */
-    @URL(message = "logo必须是一个合法的URL地址")
-    @NotEmpty(message = "logo不能为空")
+    @URL(message = "logo必须是一个合法的URL地址", groups = {AddGroup.class, UpdateGroup.class})
+    @NotEmpty(message = "logo不能为空", groups = {AddGroup.class})
 	private String logo;
 	/**
 	 * 介绍
@@ -49,14 +53,14 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 检索首字母
 	 */
-    @NotBlank(message = "首字母不能为空")
-    @Pattern(regexp = "^[a-zA-Z]$", message = "首字母必须是一个字母")
+    @NotBlank(message = "首字母不能为空", groups = {AddGroup.class})
+    @Pattern(regexp = "^[a-zA-Z]$", message = "首字母必须是一个字母", groups = {AddGroup.class, UpdateGroup.class})
 	private String firstLetter;
 	/**
 	 * 排序
 	 */
-    @NotNull
-    @Min(value = 0, message = "排序必须大于等于0")
+    @NotNull(groups = {AddGroup.class})
+    @Min(value = 0, message = "排序必须大于等于0", groups = {AddGroup.class, UpdateGroup.class})
 	private Integer sort;
 
 }
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/exception/GulimallExceptionControllerAdvice.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/exception/GulimallExceptionControllerAdvice.java
@@ -1,25 +1,47 @@
 package com.atguigu.gulimall.product.exception;
 
+import com.atguigu.common.exception.BizCodeEnume;
+import com.atguigu.common.utils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * 功能描述
+ * 功能描述: 处理全局异常
  *
  * @author: Gxf
  * @date: 2025年12月25日 11:56
  */
-@ControllerAdvice(basePackages = "com.atguigu.gulimall.product.controller")
+@Slf4j
+@RestControllerAdvice(basePackages = "com.atguigu.gulimall.product.controller")
+//@ControllerAdvice(basePackages = "com.atguigu.gulimall.product.controller")
 public class GulimallExceptionControllerAdvice {
 
     /**
      * 处理校验异常
      * @param e
      */
-    @ExceptionHandler(value = Exception.class)
-    public void handleValidationException(Exception e){
-        e.printStackTrace();
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public R handleValidationException(MethodArgumentNotValidException e){
+        BindingResult bindingResult = e.getBindingResult();
+        // 校验失败，提取错误信息
+        Map<String, String> errorMap = new HashMap<>();
+        bindingResult.getFieldErrors().forEach(fieldError -> {
+            errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+        return R.error(BizCodeEnume.VAILD_EXCEPTION.getCode(), BizCodeEnume.VAILD_EXCEPTION.getMsg()).put("data", errorMap);
+    }
+
+    @ExceptionHandler(value = Throwable.class)
+    public R handleException(Throwable throwable){
+        return R.error(BizCodeEnume.UNKNOW_EXCEPTION.getCode(), BizCodeEnume.UNKNOW_EXCEPTION.getMsg());
     }
 
 }
