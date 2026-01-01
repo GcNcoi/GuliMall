ed80f4dbe833e1a23ba82020c1f3953ec9158a25
15506957109 <2284467180@qq.com>
2026-01-01 17:56:19 +0800

    保存SPU基本信息

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SpuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SpuInfoServiceImpl.java
@@ -1,6 +1,9 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import com.atguigu.gulimall.product.vo.SpuSaveVo;
 import org.springframework.stereotype.Service;
+
+import java.beans.Transient;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -11,6 +14,7 @@ import com.atguigu.common.utils.Query;
 import com.atguigu.gulimall.product.dao.SpuInfoDao;
 import com.atguigu.gulimall.product.entity.SpuInfoEntity;
 import com.atguigu.gulimall.product.service.SpuInfoService;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("spuInfoService")
@@ -24,6 +28,26 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    @Transactional
+    public void saveSpuInfo(SpuSaveVo spuSaveVo) {
+        // 1.保存spu基本信息:pms_spu_info
+
+        // 2.保存spu的描述图片:pms_spu_info_desc
+
+        // 3.保存spu的图片集:pms_spu_images
+
+        // 4.保存spu的规格参数:pms_product_attr_value
+
+        // 5.保存spu的积分信息:gulimall_sms->sms_spu_bounds
+
+        // 6.保存当前spu对应的sku信息
+        // 6.1.sku的基本信息:pms_sku_info
+        // 6.2.sku的图片信息:pms_sku_images
+        // 6.3.sku的销售属性信息:pms_sku_sale_attr_value
+        // 6.4.sku的优惠、满减等信息:gulimall_sms->sms_sku_ladder\sms_sku_full_reduction\sms_member_price
     }
 
 }
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/Bounds.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/Bounds.java
@@ -3,28 +3,20 @@
   */
 package com.atguigu.gulimall.product.vo;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
 /**
  * Auto-generated: 2025-12-31 17:46:57
  *
  * @author bejson.com (i@bejson.com)
  * @website http://www.bejson.com/java2pojo/
  */
+@Data
 public class Bounds {
 
-    private int buyBounds;
-    private int growBounds;
-    public void setBuyBounds(int buyBounds) {
-         this.buyBounds = buyBounds;
-     }
-     public int getBuyBounds() {
-         return buyBounds;
-     }
-
-    public void setGrowBounds(int growBounds) {
-         this.growBounds = growBounds;
-     }
-     public int getGrowBounds() {
-         return growBounds;
-     }
+    private BigDecimal buyBounds;
+    private BigDecimal growBounds;
 
 }
